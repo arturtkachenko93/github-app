dd0b51776660c86f220a24ec56de5e7b68a37dcf
Archibracho <artur_terner@mail.ru>
2022-01-10 13:31:28 +0300

    fixed input autocomplite, remove listeners

--- js/app.js
+++ js/app.js
@@ -1,66 +1,81 @@
-const form = document.querySelector('form');
-const formWrapper = form.querySelector('.form-wrapper');
-const formInput = form.querySelector('.input');
-const PER_PAGE = 5;
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('form');
+  const formWrapper = form.querySelector('.form-wrapper');
+  const resWrapper = form.querySelector('.res-wrapper');
+  const formInput = form.querySelector('.input');
+  const PER_PAGE = 5;
 
-function debounce(fn, ms) {
-  let timeout;
+  // задержка для ввода в поле поиска
+  function debounce(fn, ms) {
+    let timeout;
 
-  return function() {
-    const fnCall = () => { fn.apply(this, arguments)}
-    clearTimeout(timeout);
-    timeout = setTimeout(fnCall, ms);
-  }
-}
-
-function requestRepositories(e) {
-  const valueInput = e.target.value;
-  formWrapper.innerHTML = '';
-
-  if (valueInput.length === 0) {
-    formWrapper.insertAdjacentHTML('beforeend', `<span>Введите данные!</span>`);
-    return;
+    return function() {
+      const fnCall = () => { fn.apply(this, arguments)}
+      clearTimeout(timeout);
+      timeout = setTimeout(fnCall, ms);
+    }
   }
 
-  fetch(`https://api.github.com/search/repositories?q=${valueInput}&per_page=${PER_PAGE}`)
-    .then(response => {
-      if (!response.ok) {
-        throw('Ошибка данных!');
-      }
-      const data = response.json();
-      return data;
-    })
-    .then(data => {
-      data.items.forEach(el => {
-        formWrapper.insertAdjacentHTML('beforeend', `<div class='output-link' data-attr="${el.id}">${el.name}</div>`);
+  // запрос к API
+  function requestRepositories(e) {
+    const valueInput = e.target.value;
+    if (valueInput.length === 0) {
+      formWrapper.innerHTML = '';
+      formWrapper.insertAdjacentHTML('beforeend', `<span>Введите данные!</span>`);
+      return;
+    }
+
+    fetch(`https://api.github.com/search/repositories?q=${valueInput}&per_page=${PER_PAGE}`)
+      .then(response => {
+        if (!response.ok) {
+          throw('Ошибка данных!');
+        }
+        const data = response.json();
+        return data;
       })
-      const formOutputLinks = form.querySelectorAll('.output-link');
-      [...formOutputLinks].forEach(el => {
-        el.addEventListener('click', (e) => {
-          const res = data.items.find(item => item.id === +el.dataset.attr);
-          formWrapper.insertAdjacentHTML('beforeend', `<div class='output-res'>
-          <span>Name: ${res.name}</span>
-          <span class='close'>X</span>
-          <span>Owner: ${res.owner.login}</span>
-          <span>Stars: ${res.stargazers_count}</span>`);
-          formInput.value = '';
+      .then(data => {
+        data.items.forEach(el => {
+          formWrapper.insertAdjacentHTML('beforeend', `<div class='output-link' data-attr="${el.id}">${el.name}</div>`);
         })
+        // hundler по клику на один из элементов автокомплита
+        const clickOutput = (e) => {
+          const link = e.target.closest('.output-link');
+          if (link) {
+            const res = data.items.find(item => item.id === +link.dataset.attr);
+            resWrapper.insertAdjacentHTML('beforeend', `<div class='output-res'>
+            <span>Name: ${res.name}</span>
+            <span class='close'>X</span>
+            <span>Owner: ${res.owner.login}</span>
+            <span>Stars: ${res.stargazers_count}</span>`);
+            formInput.value = '';
+            formWrapper.innerHTML = '';
+            // удаление обработчика по клику на элемент
+            formWrapper.removeEventListener('click', clickOutput);
+          }
+        }
+        formWrapper.addEventListener('click', clickOutput);
       })
-    })
-    .then(() => {
-      formWrapper.addEventListener('click', (e) => {
-        if (e.target.className != 'close') return;
+      .then(() => {
+        // hundler по нажатию кнопки "закрыть" по репозиторию
+        const removeRes = (e) => {
+          if (e.target.className != 'close') return;
 
-        let out = e.target.closest('.output-res');
-        out.remove();
+          let out = e.target.closest('.output-res');
+          out.remove();
+          // удаление обработчика событий кнопки "закрыть"
+          if (btnsClose.length === 0) {
+            resWrapper.removeEventListener('click', removeRes)
+          }
+        }
+
+        resWrapper.addEventListener('click', removeRes)
       })
-    })
-    .catch(e => {
-      formWrapper.insertAdjacentHTML('beforeend', `<span>${e}</span>`);
-      console.log(e);
-    });
-}
+      .catch(e => {
+        formWrapper.insertAdjacentHTML('beforeend', `<span>${e}</span>`);
+      });
+  }
 
-requestRepositories = debounce(requestRepositories, 500)
+  requestRepositories = debounce(requestRepositories, 500)
 
-formInput.addEventListener('keyup', requestRepositories);
+  formInput.addEventListener('keyup', requestRepositories);
+})
